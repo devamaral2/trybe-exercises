52d03240d4a5cc805d37e1ce37a665a8978073f4
Rafael Amaral <amaral.avelar.filo@gmail.com>
2021-11-20 14:24:01 -0300

    entrega dos exercicios 8.2

--- fundamentos/bloco-8-higher-order-function-do-javascript-ES6/aula-2-Higher-order-functions-toEach-find-some-every-sort/exercicie1.js
+++ fundamentos/bloco-8-higher-order-function-do-javascript-ES6/aula-2-Higher-order-functions-toEach-find-some-every-sort/exercicie1.js
@@ -61,23 +61,71 @@ const books = [
     },
   ];
 
-  function authorBornIn1947(objeto) {
-    if (objeto.author.birthYear === 1947) {
-      console.log(objeto.author.name)
-    };
-};
-  books.find(authorBornIn1947);
+  // function authorBornIn1947(objeto) {
+    // if (objeto.author.birthYear === 1947) {
+      // console.log(objeto.author.name)
+    // };
+// };
+  // books.find(authorBornIn1947);
+  
 
-  function smallerName(objeto, indice) {
-    
-    
-    let nameBook;
-      
-    return nameBook;
+  function smallerName() {
+    let nameBook = books[0].name;
+    // escreva aqui o seu código
+  
+    books.forEach((book) => {
+      if (book.name.length < nameBook.length) {
+        nameBook = book.name;
+      }
+    });
+    console.log(nameBook);
   }
+  smallerName()
 
-  console.log(books.forEach(smallerName));
 
 
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  // Adiciona seu código aqui
+  return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+}
+
+console.log(findDivisibleBy3And5())
+
+function getNamedBook() {
+   
+   
+   return books.find ((book) => book.name.length === 26)
+}
+
+console.log(getNamedBook())
+
+function booksOrderedByReleaseYearDesc() {
+  return books.sort((a, b) => b.releaseYear - a.releaseYear);
 
   
+  
+}
+console.log(booksOrderedByReleaseYearDesc())
+
+
+function everyoneWasBornOnSecXX() {
+  return books.every((book) => book.author.birthYear > 1900)
+}
+console.log (everyoneWasBornOnSecXX())
+
+
+
+function someBookWasReleaseOnThe80s() {
+  return books.some ((book) => book.releaseYear > 1979 && book.releaseYear < 1990)
+}
+console.log(someBookWasReleaseOnThe80s())
+
+function authorUnique() {
+  return books.every((book) =>
+    !books.some((bookSome) =>
+      (bookSome.author.birthYear === book.author.birthYear)
+      && (bookSome.author.name !== book.author.name)));
+}
+console.log (authorUnique())
