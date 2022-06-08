9912724a5421703c7834286f680e6fdc9d667ddb
Rafael Amaral <amaral.avelar.filo@gmail.com>
2022-06-08 15:17:48 -0300

    final

---
+++ dist/characters.js
@@ -0,0 +1,33 @@
+"use strict";
+var readlineSync = require('readline-sync');
+const characters = [
+    {
+        nickname: 'xKillerx',
+        class: 'warrior',
+        stats: { agi: 50, str: 100, int: 25, hp: 1000, mp: 300 },
+        createdAt: new Date('2003-10-1')
+    },
+    {
+        nickname: 'jainaProud',
+        class: 'mage',
+        stats: { agi: 80, str: 40, int: 150, hp: 630, mp: 1100 },
+        createdAt: new Date('2003-10-2')
+    },
+    {
+        nickname: 'catIn',
+        class: 'hunter',
+        stats: { agi: 150, str: 90, int: 80, hp: 800, mp: 600 },
+        createdAt: new Date('2003-10-15')
+    },
+];
+// function printCharacter(character, index) {
+//     const { nickname, class: cls, createdAt } = character;
+//     console.log(`\n\n===== Character: ${index + 1} ========`);
+//     console.log(`nickname: ${nickname}
+// class: ${cls}
+// createdAt: ${createdAt}`);
+// }
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+// characters.forEach(printCharacter);
+
